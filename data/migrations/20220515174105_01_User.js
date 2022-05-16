0e939149b3055226759949d40f09d2fe5d4d49f9
Spencer <spenclark@hotmail.com>
2022-05-16 11:13:07 -0700

    user schema

--- data/migrations/20220515174105_01_User.js
+++ data/migrations/20220515174105_01_User.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+  knex.schema.createTable("users", function (table) {
+    table.uuid("id");
+    table.timestamp("created_at", { precision: 6 }).defaultTo(knex.fn.now(6));
+    table.string("primaryEmail");
+    table.string("password");
+    table.string("website");
+    table.string("bio");
+    table.string("employee_count");
+    table.string("hq_location");
+  });
 };
 
 /**
@@ -11,5 +20,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("users");
 };
