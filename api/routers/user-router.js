e47f5e0024a2a5f9283ba7a07fb31a0c3529efdd
Spencer <spenclark@hotmail.com>
2022-05-17 13:19:09 -0700

    login

--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -1,3 +1,27 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+
+const Users = require("../users/user-model");
+const Token = require("../helpers/auth-helpers.js");
+
+router.post("/login", (req, res) => {
+  let { email, password } = req.body;
+
+  Users.findBy({ email })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = Token.getJwt(user.email);
+        res.status(200).json({
+          id: user.id,
+          email: user.email,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
