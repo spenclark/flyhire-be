7a2ad1f6e873b363dd0e8deadbfd82e183cc8fb6
Spencer <spenclark@hotmail.com>
2022-05-17 13:20:45 -0700

    user router

--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -25,3 +25,29 @@ router.post("/login", (req, res) => {
       res.status(500).json(error);
     });
 });
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+
+  //   const validateResult = validateUser(user);
+
+  //   if (validateResult.isSuccessful === true) {
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  const token = Token.getJwt(user.email);
+
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json({ id: saved.id, email: saved.email, token: token });
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+  //   } else {
+  res.status(400).json({
+    message: "Invalid user info, see errors",
+    errors: validateResult.errors,
+  });
+  //   }
+});
