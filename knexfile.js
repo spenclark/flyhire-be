2aca45764ed35ed5261c24c330a26578e6e2a8e5
Spencer <spenclark@hotmail.com>
2022-05-18 14:49:35 -0700

    heroku

--- knexfile.js
+++ knexfile.js
@@ -38,7 +38,7 @@ module.exports = {
   },
 
   production: {
-    client: "pg",
+    client: "postgres",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
