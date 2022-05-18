dc28997f8e365ca1ee20d4bf9c0c8b702a74e9c3
Spencer <spenclark@hotmail.com>
2022-05-18 14:54:34 -0700

    postgres

--- knexfile.js
+++ knexfile.js
@@ -21,24 +21,8 @@ module.exports = {
     max: 10,
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
   production: {
-    client: "postgres",
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
