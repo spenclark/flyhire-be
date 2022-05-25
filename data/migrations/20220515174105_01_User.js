7ab9352251fe89a2d2cfcb6fe993a09ffe90425c
Spencer <spenclark@hotmail.com>
2022-05-24 22:07:46 -0700

    this should be a uuid tbh

--- data/migrations/20220515174105_01_User.js
+++ data/migrations/20220515174105_01_User.js
@@ -5,7 +5,7 @@
 exports.up = function(knex) {
   return  knex.schema.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"').createTable("users", function (table) {
     table
-      .string("id")
+      .uuid("id")
       .primary()
       .unique()
       .defaultTo(knex.raw("uuid_generate_v4()"));
